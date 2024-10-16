20a78856f27ba6d872e45ca4cca578d89d4dd677
slliynote <slliynotenote@gmai.com>
2024-10-16 20:08:01 -0300

    react

---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+//import Image from "next/image";
+//import {inter} from "next/font/local";
+
+//const Inter = inter({ subsets: ['latin']})
+
+
+
+export function topo(){
+  return(
+    <main>
+      <h1> topo da pagina </h1>
+    </main>
+  )
+};
+
+export default function Home() {
+  return (
+
+      <main>
+      <div>{topo()}</div>
+      <h2>noticias aqui</h2>
+      <button>clique na noticia</button>
+      <div>{rodapé()}</div>
+     </main>
+  )
+};
+
+export function rodapé(){
+  return(
+    <main>
+      <h1> final da pagina</h1>
+    </main>
+  )
+};
