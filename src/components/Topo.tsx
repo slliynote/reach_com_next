ee76889989598220ff2f8c9b5c66085d33e277c9
slliynote <slliynotenote@gmai.com>
2024-10-18 22:03:25 -0300

    alteração

---
+++ src/components/Topo.tsx
@@ -0,0 +1,7 @@
+export function Topo(){
+    return(
+      <main>
+        <h1> topo da pagina </h1>
+      </main>
+    )
+  };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,20 +4,14 @@
 //const Inter = inter({ subsets: ['latin']})
 
 
+import { Topo } from "@/components/Topo"
 
-export function topo(){
-  return(
-    <main>
-      <h1> topo da pagina </h1>
-    </main>
-  )
-};
 
 export default function Home() {
   return (
 
       <main>
-      <div>{topo()}</div>
+        <Topo/>
       <h2>noticias aqui</h2>
       <div>testando o git</div>
       <button>clique na noticia</button>
